436564d3d242e163d36e089cd73cb7abfc9c0240
MtkN1 <51289448+MtkN1@users.noreply.github.com>
2024-12-05 05:49:17 +0000

    first commit

---
+++ tests/test_skip.py
@@ -0,0 +1,16 @@
+import pytest
+
+
+@pytest.fixture
+def docker_client() -> object:
+    try:
+        # NOTE: Actually connect with the docker sdk
+        raise Exception("Docker client not available")
+    except Exception:
+        pytest.skip("Docker client not available")
+
+    return object()
+
+
+def test_hang(docker_client):
+    assert ...
